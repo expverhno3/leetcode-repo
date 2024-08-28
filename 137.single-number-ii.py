4feac22bb9ae6304a2a8d6a2960a8538ef9d865c
expverhno3 <flyed666@outlook.com>
2024-08-28 16:14:12 +0800

    ✨ feat(bit op): finite state machine
    reimpl single number with finite state machine: truth table -> k-map -> expression

--- 137.single-number-ii.py
+++ 137.single-number-ii.py
@@ -9,11 +9,29 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ones, twos = 0, 0
+        # --- finite state machine, my impl
+
+        #NOTE: init
+        Q0, Q1 = 0, 0
+
+        # define update rule
+        def update(Q0, Q1, x):
+            D0 = (~x & Q0) | (~Q1 & Q0) | (~Q1 & x)
+            D1 = (Q1 & Q0 & ~x) | (~Q1 & Q0 & x)
+            return D0, D1
+        
         for num in nums:
-            ones = ones ^ num & ~twos
-            twos = twos ^ num & ~ones
-        return ones
+            Q0, Q1 = update(Q0, Q1, num)
+        
+        return ~Q1 & Q0
+
+
+        # --- answer from leetcode solutions
+        # ones, twos = 0, 0
+        # for num in nums:
+        #     ones = ones ^ num & ~twos
+        #     twos = twos ^ num & ~ones
+        # return ones
         
 # @lc code=end
 
